0bde63087c68a9747c429baf7ade099c5cc4f2dd
Faisal <faisal_a@live.co.uk>
2017-07-31 19:03:58 +0100

    cats have been added to browser page using addCat function defined

--- public/app.js
+++ public/app.js
@@ -68,7 +68,14 @@ var appendElements = function(li1, li2, li3) {
 }
 
 var app = function() {
-  addCat("Tails", "Dreamies", "http://cdn.playbuzz.com/cdn/0079c830-3406-4c05-a5c1-bc43e8f01479/7dd84d70-768b-492b-88f7-a6c70f2db2e9.jpg")
+  addCat("Boba", "Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
+
+  addCat("Barnaby", "Tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
+
+  addCat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
+
+  addCat("Tails", "Dreamies", "http://cdn.playbuzz.com/cdn/0079c830-3406-4c05-a5c1-bc43e8f01479/7dd84d70-768b-492b-88f7-a6c70f2db2e9.jpg");
+
 }
 
 window.addEventListener('load', app);
